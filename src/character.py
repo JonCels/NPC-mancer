5f6699a48acc526057fe5b629b6dcb24ab961edb
Jonathan Cels <celsj@mcmaster.ca>
2021-06-07 18:53:30 -0400

    Starting adding racial features

--- src/character.py
+++ src/character.py
@@ -12,6 +12,7 @@ selectableClassSpecs = []
 skillsList = []
 backgroundsList = []
 armoursList = []
+weaponsList = []
 
 numProf = 18
 numStat = 6
@@ -28,11 +29,14 @@ class Character():
         self.subrace = race[1]
         self.dndClass = dndClass[0]['class']
         self.dndClassSpec = dndClass[1]['class']
+        self.subclass = self.selectSubclass(dndClass)
         #subclass
         self.background = background['background']
         self.level = level
-
+        
+        self.prioDict = {}
         self.stats = self.placeStats()
+        self.racialTraits()
         self.modifiers = self.calcModifiers()
 
         self.skills = self.placeSkills()
@@ -51,12 +55,18 @@ class Character():
         self.backgroundTraits(background)
         self.classTraits(dndClass)
 
+        self.weapons = []
+
+    def selectSubclass(self, dndClass):
+        pass
+
     #Generates an array of ability scores and places them according to common ability priorities based on the character class
     def placeStats(self):
         statArray = generateStatArray()
         #priorityArray = generatePriorityArray(self.class)
         #todo: generatePriorityDict function
-        prioDict = {'Strength' : 5, 'Dexterity': 0, 'Constitution': 2, 'Intelligence': 3, 'Wisdom': 1, 'Charisma': 4}
+        self.prioDict = {'Strength' : 5, 'Dexterity': 0, 'Constitution': 2, 'Intelligence': 3, 'Wisdom': 1, 'Charisma': 4}
+        prioDict = self.prioDict.copy()
         stats = {}
 
         for _ in range(numStat):
@@ -64,7 +74,7 @@ class Character():
             currAbility = min(prioDict, key=prioDict.get)
             prioDict.pop(currAbility)
             
-            #Get next highest score
+            #Get next highest scores
             currScore = max(statArray)
             statArray.remove(currScore)
 
@@ -117,23 +127,205 @@ class Character():
         return priority
 
     def racialTraits(self):
-        pass
-        #name = self.race['name']
-        #if ('Elf' in name):
+        if (self.race == "Aarakocra"):
+            self.stats['Dexterity'] += 2
+            self.stats['Wisdom'] += 1
+            #Flight
+            #Talons
+            #Languages
+
+        elif (self.race == "Aasimar"):
+            self.stats['Charisma'] += 2
             #Darkvision
-            #Fey Ancestry
+            #Celestial Resistance
+            #Healing Hands
+            #Light Bearer
+            #Languages
+            if (self.subrace == "Protector Aasimar"):
+                self.stats['Wisdom'] += 1
+                #Radiant soul
+            elif (self.subrace == "Scourge Aasimar"):
+                self.stats['Constitution'] += 1
+                #Radiant consumption
+            elif (self.subrace == "Fallen Aasimar"):
+                self.stats['Strength'] += 1
+                #Necrotic shroud
+        
+        elif (self.race == "Bugbear"):
+            self.stats['Strength'] += 2
+            self.stats['Dexterity'] += 1
+            #Darkvision
+            #Long limbed
+            #Powerful build
+            #Sneaky
+            #Surprise attack
+            #Languages
+        
+        elif (self.race == "Dragonborn"):
+            self.stats['Strength'] += 2
+            self.stats['Charisma'] += 1
+            #Draconic ancestry
+            #Breath weapon
+            #Damage resistance
+            #Languages
+
+        elif (self.race == "Dwarf"):
+            self.stats['Constitution'] += 2
+            #Darkvision
+            #Dwarven resilience
+            #Dwarven combat training
+            #Tool proficicency
+            #Stonecunning
+            #Languages
+
+            if (self.subrace == "Hill Dwarf"):
+                self.stats['Wisdom'] += 1
+                #Dwarven toughness
+
+            elif (self.subrace == "Mountain Dwarf"):
+                self.stats['Strength'] += 2
+                #Dwarven armor training
+
+        elif (self.race == "Elf"):
+            self.stats['Dexterity'] += 2
+            #Darkvision
+            #Keen senses
+            #Fey ancestry
             #Trance
             #Languages
-            #if ('High' in name):
-                #Elf Weapon Training
+
+            if (self.subrace == "High Elf"):
+                self.stats['Intelligence'] += 1
+                #Elf weapon training
                 #Cantrip
                 #Extra language
-                #pass
-            #elif ('Wood' in name):
-                #Elf Weapon Training
-                #Fleet of Foot
-                #Mask of the Wild
-                #pass
+
+            elif (self.subrace == "Wood Elf"):
+                self.stats['Wisdom'] += 1
+                #Elf weapon training
+                #Fleet of foot
+                #Mask of the wild
+        
+        elif (self.race == "Firbolg"):
+            self.stats['Wisdom'] += 2
+            self.stats['Strength'] += 1
+            #Firbolg magic
+            #Hidden step
+            #Powerful build
+            #Speech of beast and leaf
+            #Languages
+        
+        elif (self.race == "Genasi"):
+            self.stats['Constitution'] += 2
+            #Languages
+
+            if (self.subrace == "Air Genasi"):
+                self.stats['Dexterity'] += 1
+                #Unending breath
+                #Mingle with the wind
+            
+            elif (self.subrace == "Earth Genasi"):
+                self.stats['Strength'] += 1
+                #Earth walk
+                #Merge with stone
+            
+            elif (self.subrace == "Fire Genasi"):
+                self.stats['Intelligence'] += 1
+                #Darkvision
+                #Fire resistance
+                #Reach to the blaze
+
+            elif (self.subrace == "Water Genasi"):
+                self.stats['Wisdom'] += 1
+                #Acid Resistance
+                #Amphibious
+                #Swim
+                #Call to the wave
+        
+        elif (self.race == "Gnome"):
+            self.stats['Intelligence'] += 2
+            #Darkvision
+            #Gnome cunning
+            #Languages
+
+            if (self.subrace == "Forest Gnome"):
+                self.stats['Dexterity'] += 1
+                #Natural Illusionist
+                #Speak with small beasts
+            
+            elif (self.subrace == "Rock Gnome"):
+                self.stats['Constitution'] += 1
+                #Artificers lore
+                #Tinker
+            
+            elif (self.subrace == "Deep Gnome"):
+                self.stats['Dexterity']
+                #Superior darkvision
+                #Stone camouflage
+        
+        elif (self.race == "Goblin"):
+            self.stats['Dexterity'] += 2
+            self.stats['Constitution'] += 1
+            #Darkvision
+            #Fury of the small
+            #Nimble escape
+            #Languages
+        
+        elif (self.race == "Goliath"):
+            self.stats['Strength'] += 2
+            self.stats['Constitution'] += 1
+            #Natural athlete
+            #Stone's endurance
+            #Powerful build
+            #Mountain born
+        
+        elif (self.race == "Half-Elf"):
+            self.stats['Charisma'] += 2
+
+            #Choose based on abilityPriority
+            prioDict = self.prioDict.copy()
+            prioDict.pop("Charisma")
+
+            sortedPrioDict = dict(sorted(prioDict.items(), key=lambda item: item[1]))
+            focus1 = list(sortedPrioDict.items())[:1][0][0]
+            focus2 = list(sortedPrioDict.items())[1:2][0][0]
+            self.stats[focus1] += 1
+            self.stats[focus2] += 1
+        
+        elif (self.race == "Half-Orc"):
+            self.stats['Strength'] += 2
+            self.stats['Constitution'] += 1
+            #Darkvision
+            #Menacing
+            #Relentless endurance
+            #Savage attacks
+            #Languages
+        
+        elif (self.race == "Halfling"):
+            self.stats['Dexterity'] += 2
+            
+            if (self.subrace == "Lightfoot Halfling"):
+                self.stats['Charisma'] += 1
+                #Naturally stealthy
+            
+            elif (self.subrace == "Stout Halfling"):
+                self.stats['Constitution'] += 1
+                #Stout resilience
+        
+        elif (self.race == "Hobgoblin"):
+            self.stats['Constitution'] += 2
+            self.stats['Intelligence'] += 1
+            #Darkvision
+            #Martial Training
+            #Saving face
+            #Languages
+            
+
+
+
+
+
+
 
     def classTraits(self, dndClass):
         #Saving throw proficiencies
@@ -176,6 +368,10 @@ class Character():
         self.ac = ac
         self.armour = selectedArmour
         return selectedArmour
+
+    def selectWeapon(self, weapon):
+        selectedWeapon = getWeapon(weapon)
+        self.weapons.append(selectedWeapon)
 
     def levelUp(self):
         pass
@@ -294,6 +490,16 @@ def fetchArmours():
     for armour in mycursor:
         armoursList.append(armour)
 
+def fetchWeapons():
+    db = sqlConnect()
+
+    mycursor = db.cursor(dictionary=True)
+    query = "SELECT name, rolls, damage, damage_type, weapon_type, weapon_range, properties FROM weapons"
+    mycursor.execute(query)
+
+    for weapon in mycursor:
+        weaponsList.append(weapon)
+
 def sqlConnect():
     db = mysql.connector.connect(
         host="localhost",
@@ -353,17 +559,21 @@ def getBackground(name):
 def getArmour(name):
     return list(filter(lambda armour : armour['name'] == name, armoursList))[0]
 
+def getWeapon(name):
+    return list(filter(lambda weapon : weapon['name'] == name, weaponsList))[0]
+
 fetchAbilities()
 fetchSkills()
 fetchRaces()
 fetchClasses()
 fetchBackgrounds()
 fetchArmours()
+fetchWeapons()
 
-selectRace("Elf")
+selectRace("Half-Elf")
 
 fetchSubraces()
-selectSubrace("High Elf")
+#selectSubrace("Wood Elf")
 
 selectClass("Ranger")
 
@@ -372,10 +582,8 @@ selectClassSpec("Ranger (Dex)")
 
 selectBackground("Urchin")
 
-print(skillsList)
-
 steve = Character([selectedRace, selectedSubrace], [selectedClass, selectedClassSpec], selectedBackground, 9)
 steve.addSkillProficiency("Perception")
-steve.print()
+#steve.print()
 
 
